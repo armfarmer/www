85dd25268f36b9a94e777a86d9c7249017506b9b
armfarmer <73695599+armfarmer@users.noreply.github.com>
2020-10-30 18:01:34 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>introduce myself</h1>
+        <p>my name is kritphiphat thongtammarchart</p>
+        <p>my nickname is arm</p>
+        <h2>แนะนำตัว</h2>
+        <p>สวัดดีผมชื่อ กฤษณ์พิพัฒน์ ทองธรรมชาติ ชื่อเล่น อาร์ม</p>
+        <img scr="a.png">
+        <img scr="b.png">
+        <br>
+        <h2><a href="http://www.youtube.com">this is my favourite link</a></h2>
+        
+        <h1>my joke</h1>
+        <h2>เรื่องตลก</h2>
+        <p> ผมเล่นมุขไม่ตลกแล้วเพื่อนตลกกัน</p>
+
+    </body>
+</html>        
